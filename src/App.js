b87d285d345eab88e2d7b05f78c72fb9aa58db79
108towa <5b0g0013@office.stust.edu.tw>
2024-05-13 23:25:29 +0800

    hi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MySlider from "./components/MySlider";
 function App() {
   return (
     <div className="App">
-      <h1>5B0G0013，林傳鈞</h1>
+      <h1>5B0G0013，林傳鈞 第二元件</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
