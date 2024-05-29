ebb1178d12064c815da10cc32a955e3c6ae5ad78
108towa <5b0g0013@office.stust.edu.tw>
2024-05-29 20:32:11 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
-      <h1>5B0G0013，林傳鈞 第二元件</h1>
+      <h1>MySlider</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
     </div>
   );
 }
